6af292d3688ece00cab4e3f8e8fd7a8cc65c9805
Gaurav192 <gauravshetty96@gmail.com>
2019-03-30 14:23:59 +0530

    Add SliverAppBar Yeah it's 'Sliver' not 'Silver' xD Also add some description for AboutPage

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import './ui/about.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp( MaterialApp(
     title: 'Login',
     home: AboutPage(),
+    theme: ThemeData(primarySwatch: Colors.teal),
   ));
 }
--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 200,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+                background:
+                    Image.asset('assets/citrus_logo.jpg', fit: BoxFit.fitWidth),
+                title: Text('About Citrus')),
+          ),
+          FutureBuilder(
+            future:
+                DefaultAssetBundle.of(context).loadString('json/about.json'),
+            builder: (context, snapshot) {
+              var _about = json.decode(snapshot.data.toString());
+              return SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.all(10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      _about[index]['description'],
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                );
+              }, childCount: _about == null ? 0 : _about.length));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
