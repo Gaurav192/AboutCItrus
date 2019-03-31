aa2f5be065b1c7e5e2b9e5ca6cf65494f3eb26c5
Gaurav192 <gauravshetty96@gmail.com>
2019-03-31 13:14:17 +0530

    Update TeamPage

--- lib/ui/team.dart
+++ lib/ui/team.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class TeamPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-        delegate: SliverChildListDelegate([Text('Initial Widget')]));
+    return FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('json/team.json'),
+        builder: (context, snapshot) {
+          var _items = json.decode(snapshot.data.toString());
+          return SliverList(
+              delegate:
+                  SliverChildBuilderDelegate((BuildContext context, int index) {
+            return Card(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      child: Column(
+                    children: <Widget>[
+                      Image.asset(
+                        _items[index]['coverImage'],
+                        fit: BoxFit.fitWidth,
+                      ),
+                      ListTile(
+                        title: Text(_items[index]['name'],
+                            textAlign: TextAlign.center),
+                        subtitle: Text(_items[index]['role'],
+                            textAlign: TextAlign.center),
+                      )
+                    ],
+                  )),
+                ],
+              ),
+            );
+          }, childCount: _items == null ? 0 : _items.length));
+        });
   }
 }
